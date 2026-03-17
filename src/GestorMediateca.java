47f986488ec8adf9bf4e2cb86aeef00aa31adc7e
Xandre <xanresfg@gmail.com>
2026-03-17 22:42:50 +0100

    clase gestor mediateca

---
+++ src/GestorMediateca.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GestorMediateca {
+
+    static ArrayList<ItemBiblioteca> catalogo = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+}
